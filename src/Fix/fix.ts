18dd6e2911fb09d35c57fc2f751f4dad4ef7ceaf
yuzhanglong <yuzl1123@163.com>
2021-03-30 13:45:27 +0800

    refactor: update import from lint-md

--- src/Fix/fix.ts
+++ src/Fix/fix.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import { fix as runFix } from 'lint-md';
+import { fix as runFix } from '@lint-md/core';
 import { CliConfig } from '../types';
 
 /**
--- src/Lint/lint.ts
+++ src/Lint/lint.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import { lint as runLint } from 'lint-md';
+import { lint as runLint } from '@lint-md/core';
 import { CliLintResult, CliConfig } from '../types';
 
 /**
